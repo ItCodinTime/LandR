f21c67db768ae32632ec9ff9664eaff10e15ffd0
Goutham KR <140845356+ItCodinTime@users.noreply.github.com>
2025-07-07 23:44:51 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
+export default function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
